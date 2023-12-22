af53a7c8d7c72c9708583b08a1c38bd0f3ac5107
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2023-12-22 14:03:36 +0000

    initial commit

---
+++ corelib/src/pgscatalog/corelib/__init__.py
@@ -0,0 +1,5 @@
+from pgscatalog.corelib.scoringfile import ScoringFile
+
+# be explicit about public interfaces
+__all__ = ["ScoringFile"
+    ]
---
+++ downloadapp/src/pgscatalog/downloadapp/cli.py
@@ -0,0 +1,8 @@
+import pgscatalog.corelib
+
+def run():
+    test = pgscatalog.corelib.ScoringFile()
+    print(f"I imported a test object {test}")
+    
+if __name__ == "__main__":
+    run()
