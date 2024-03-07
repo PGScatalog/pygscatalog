83d08429cfdbe33e73565831f27b3e82ff0661da
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2024-03-07 09:01:00 +0000

    add export to __all__

--- pgscatalog.calclib/src/pgscatalog/calclib/__init__.py
+++ pgscatalog.calclib/src/pgscatalog/calclib/__init__.py
@@ -1,9 +1,22 @@
 import logging
 
-from .polygenicscore import PolygenicScore
+from .polygenicscore import (
+    PolygenicScore,
+    AggregatedPGS,
+    AdjustResults,
+    AdjustArguments,
+)
+from .principalcomponents import PopulationType, PrincipalComponents
 
 log_fmt = "%(name)s: %(asctime)s %(levelname)-8s %(message)s"
 logging.basicConfig(format=log_fmt, datefmt="%Y-%m-%d %H:%M:%S")
 logger = logging.getLogger(__name__)
 
-__all__ = ["PolygenicScore"]
+__all__ = [
+    "PolygenicScore",
+    "PrincipalComponents",
+    "PopulationType",
+    "AggregatedPGS",
+    "AdjustArguments",
+    "AdjustResults",
+]
