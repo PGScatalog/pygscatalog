3c3d91d0f7dff967137c700abed76f988de8b455
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2024-10-07 10:49:21 +0100

    add `verify_variants` CLI option to `pgscatalog-aggregate` (#52)
    * add CLI option to verify scores were calculated from a consistent set of variants
    
    * add missing file test
    
    * parameterise test_var_overlap_missing
    
    * add logging statements
    
    * oops: delete --variants arg
    
    * fix help message
    
    * Bump version of calc
    
    ---------
    
    Co-authored-by: Sam Lambert <sam.a.lambert@gmail.com>

--- pgscatalog.calc/src/pgscatalog/calc/cli/aggregate_cli.py
+++ pgscatalog.calc/src/pgscatalog/calc/cli/aggregate_cli.py
@@ -1,14 +1,55 @@
 import argparse
+import csv
 import logging
 import pathlib
 import textwrap
+import typing
 from collections import deque
 from typing import Optional
+from xopen import xopen
 
 from ..lib import PolygenicScore
 from pgscatalog.core import chrom_keyfunc
 
 logger = logging.getLogger(__name__)
+
+
+def get_id_from_scorefile(path: pathlib.Path) -> typing.Generator[str, None, None]:
+    """Get IDs from a scoring file, where ID is the first column"""
+    with xopen(path) as f:
+        x = csv.reader(f, delimiter="\t")
+        for i, field in enumerate(x):
+            if i == 0:
+                continue  # skip header
+            else:
+                yield field[0].strip()
+
+
+def verify_variants(score: pathlib.Path) -> None:
+    """Check that the variants used to calculate the score (.vars file) are identical to the variants in the scoring file (.scorefile.gz)
+    Raises a ValueError if the variants don't overlap perfectly"""
+    base_path: str = score.stem.split(".")[0]
+    var_path: pathlib.Path = score.parent / (base_path + ".sscore.vars")
+    scorefile_path: pathlib.Path = score.parent / (base_path + ".scorefile.gz")
+
+    if not var_path.exists():
+        raise FileNotFoundError(f"{var_path} doesn't exist")
+
+    if not scorefile_path.exists():
+        raise FileNotFoundError(f"{scorefile_path} doesn't exist")
+
+    with open(var_path) as f:
+        # just a file where each line contains a variant ID
+        scored_variants = frozenset(x.strip() for x in f)
+        logger.info(f"Read {len(scored_variants)} from {var_path}")
+
+    scorefile_variants = frozenset(get_id_from_scorefile(scorefile_path))
+    logger.info(f"Read {len(scorefile_variants)} from {scorefile_path}")
+
+    if diff := scored_variants.symmetric_difference(scorefile_variants):
+        raise ValueError(f"Missing variants {diff}")
+    else:
+        logger.info(f"{score} was calculated from good variant sets :)")
 
 
 def run_aggregate():
@@ -29,6 +70,11 @@ def run_aggregate():
         raise FileNotFoundError(f"--outdir {outdir.name} doesn't exist")
 
     score_paths = sorted([pathlib.Path(x) for x in args.scores], key=chrom_keyfunc())
+
+    if args.verify_variants:
+        logger.info("Checking variant overlap")
+        [verify_variants(x) for x in score_paths]
+
     # dfs are only read into memory after accessing them explicitly e.g. pgs[0].df
     pgs = deque(PolygenicScore(path=x) for x in score_paths)
 
@@ -119,6 +165,14 @@ def _parse_args(args=None):
         dest="verbose",
         action="store_true",
         help="<Optional> Extra logging information",
+    )
+    parser.add_argument(
+        "--verify_variants",
+        dest="verify_variants",
+        action="store_true",
+        help="<Optional> Verify variants from scoring file match scored variants perfectly."
+        " Note: requires .scorefile.gz files used by plink to create the calculated score, and a .sscore.vars file output by plink after scoring"
+        " It's assumed that these files have a common file name prefix (this is default plink behaviour) e.g. cineca_22_additive_0.sscore.zst needs cineca_22_additive_0.scorefile.gz & cineca_22_additive_0.sscore.vars",
     )
     return parser.parse_args(args)
 
--- pgscatalog.calc/tests/test_aggregate.py
+++ pgscatalog.calc/tests/test_aggregate.py
@@ -1,5 +1,7 @@
 import glob
 import itertools
+import shutil
+from os import unlink
 from unittest.mock import patch
 
 from pgscatalog.calc.cli.aggregate_cli import run_aggregate
@@ -62,3 +64,127 @@ def test_nosplit_aggregate(tmp_path_factory, scorefiles, expected_output_columns
     outdf = pd.read_csv(outf[0], sep="\t")
     assert list(outdf.columns) == expected_output_columns
     assert outdf.shape == (929, len(expected_output_columns))
+
+
+@pytest.fixture(scope="module")
+def calculated_scores(request):
+    """Calculated scores (split by effect type)"""
+    basedir = request.path.parent / "data" / "aggregate"
+    return glob.glob(str(basedir / "*sscore.zst"))
+
+
+@pytest.fixture(scope="module")
+def score_files(request):
+    """Score variable reports from plink2"""
+    basedir = request.path.parent / "data" / "aggregate"
+    return glob.glob(str(basedir / "*scorefile.gz"))
+
+
+@pytest.fixture(scope="module")
+def score_vars(request):
+    """Scoring files produced by pgscatalog-combine"""
+    basedir = request.path.parent / "data" / "aggregate"
+    return glob.glob(str(basedir / "*sscore.vars"))
+
+
+def test_var_overlap(tmp_path_factory, calculated_scores, score_vars, score_files):
+    outdir = tmp_path_factory.mktemp("outdir")
+
+    args = [
+        (
+            "pgscatalog-aggregate",
+            "-s",
+            *calculated_scores,
+            "--outdir",
+            str(outdir),
+            "--no-split",
+            "--verify_variants",
+        )
+    ]
+    flargs = list(itertools.chain(*args))
+
+    with patch("sys.argv", flargs):
+        run_aggregate()
+
+    outf = list(outdir.glob("*.txt.gz"))
+    assert [x.name for x in outf] == ["aggregated_scores.txt.gz"]
+
+
+@pytest.mark.parametrize(
+    "use_score_vars, use_score_files",
+    [
+        (True, False),  # Use score_vars but not score_files
+        (False, True),  # Use score_files but not score_vars
+    ],
+)
+def test_var_overlap_missing(
+    tmp_path_factory,
+    calculated_scores,
+    score_vars,
+    score_files,
+    use_score_files,
+    use_score_vars,
+):
+    outdir = tmp_path_factory.mktemp("outdir")
+    # stage data to outdir
+    copied_calculated_scores = [shutil.copy(x, outdir) for x in calculated_scores]
+    copied_score_vars = [shutil.copy(x, outdir) for x in score_vars]
+    copied_score_files = [shutil.copy(x, outdir) for x in score_files]
+
+    args = [
+        (
+            "pgscatalog-aggregate",
+            "-s",
+            *copied_calculated_scores,
+            "--outdir",
+            str(outdir),
+            "--no-split",
+            "--verify_variants",
+            "--verbose",
+        )
+    ]
+    flargs = list(itertools.chain(*args))
+
+    # parameterised: delete data important for verification
+    if not use_score_vars:
+        [unlink(x) for x in copied_score_vars]
+
+    if not use_score_files:
+        [unlink(x) for x in copied_score_files]
+
+    with pytest.raises(FileNotFoundError):
+        with patch("sys.argv", flargs):
+            run_aggregate()
+
+
+def test_var_overlap_fails(
+    tmp_path_factory, calculated_scores, score_vars, score_files
+):
+    outdir = tmp_path_factory.mktemp("outdir")
+    # stage data to outdir
+    copied_calculated_scores = [shutil.copy(x, outdir) for x in calculated_scores]
+    copied_score_vars = [shutil.copy(x, outdir) for x in score_vars]
+    _ = [shutil.copy(x, outdir) for x in score_files]
+
+    with open(copied_score_vars[0], mode="at") as f:
+        f.writelines(["potato"])
+
+    args = [
+        (
+            "pgscatalog-aggregate",
+            "-s",
+            *copied_calculated_scores,
+            "--outdir",
+            str(outdir),
+            "--no-split",
+            "--verify_variants",
+            "--verbose",
+        )
+    ]
+    flargs = list(itertools.chain(*args))
+
+    with pytest.raises(ValueError) as excinfo:
+        with patch("sys.argv", flargs):
+            run_aggregate()
+
+    assert "Missing variants" in str(excinfo.value)
