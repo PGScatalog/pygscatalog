d2da93b0f918a65b9d9f1638ab39bc7c53bd323a
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2025-02-03 17:27:58 +0000

    update CI to use nox

--- pgscatalog.core/noxfile.py
+++ pgscatalog.core/noxfile.py
@@ -8,6 +8,7 @@ nox.options.default_venv_backend = "uv"
 @nox.session
 @nox.parametrize("python", ["3.12", "3.11", "3.10"])
 def tests(session):
+    """Run pytest for all supported python versions"""
     session.run_install(
         "uv",
         "sync",
@@ -17,7 +18,19 @@ def tests(session):
 
 
 @nox.session
+def coverage(session):
+    """Run pytest and output a coverage report"""
+    session.run_install(
+        "uv",
+        "sync",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
+    session.run("pytest", "--ignore", "noxfile.py", "--cov", "--cov-report", "xml")
+
+
+@nox.session
 def lint(session):
+    """Run linting checks"""
     # https://nox.thea.codes/en/stable/cookbook.html#using-a-lockfile
     # note: uv is set up to install the test and lint dependency groups
     session.run_install(
